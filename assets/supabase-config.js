0e804402bbdd91e2e4437fce234a0a8c9665fb04
RayanPythonMC <rayan12rezaei@gmail.com>
2026-09-01 16:32:04 +0330

    Create supabase-config.js

---
+++ assets/supabase-config.js
@@ -0,0 +1,5 @@
+// مقادیر پروژه Supabase خودت را اینجا قرار بده.
+window.SUPABASE_CONFIG = {
+  url: "https://drcsczalavefylepnays.supabase.co",
+  anonKey: "sb_publishable_ptTuQFY53jF4F6g7dUqFVA_k7a-kl-8"
+};
